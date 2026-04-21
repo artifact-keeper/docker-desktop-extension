8b902f2a2c31e607c878d0326bd961c8a543dd6c
Brandon Geraci <brandon.geraci@gmail.com>
2026-04-20 19:23:03 -0500

    chore: bump extension to 1.0.7

--- backend/main.go
+++ backend/main.go
@@ -17,7 +17,7 @@ import (
 const (
 	configPath  = "/data/config/config.json"
 	secretsPath = "/data/config/secrets.json"
-	version     = "1.0.6"
+	version     = "1.0.7"
 )
 
 var logger = logrus.New()
